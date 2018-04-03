b7d6b328b440c452a651fee2bd161a02469962cb
wormstorm <wormstorm@users.noreply.github.com>
2018-04-03 15:31:06 +0200

    Inicio practica 2
    Añadido el cromosomaInteger unico, y el cromosomaPR2

---
+++ src/cromosoma/CromosomaPr2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cromosoma;
+
+/**
+ *
+ * @author usuario_local
+ */
+public class CromosomaPr2 extends CromosomaIntegerUnico {
+
+    @Override
+    public double[] fenotipo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double aptitud() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
